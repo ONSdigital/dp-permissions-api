eb6f18398e0515e6018d93dbf4e9e8a564ae6669
Mariana Silva <Marianasilvago1234@gmail.com>
2021-11-17 22:01:15 +0530

    Fix update query

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -3,6 +3,7 @@ package mongo
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/ONSdigital/dp-healthcheck/healthcheck"
 	dpMongoHealth "github.com/ONSdigital/dp-mongodb/v3/health"
@@ -191,7 +192,14 @@ func (m *Mongo) GetPolicy(ctx context.Context, id string) (*models.Policy, error
 
 func (m *Mongo) UpdatePolicy(ctx context.Context, policy *models.Policy) (*models.UpdateResult, error) {
 	log.Info(ctx, "update policy by id", log.Data{"id": policy.ID})
-	upsertResult, err := m.Connection.C(m.PoliciesCollection).UpsertById(ctx, policy.ID, policy)
+	updatePolicy := bson.M{
+		"$set": policy,
+		"$setOnInsert": bson.M{
+			"last_updated": time.Now(),
+		},
+	}
+
+	upsertResult, err := m.Connection.C(m.PoliciesCollection).UpsertById(ctx, policy.ID, updatePolicy)
 	if err != nil {
 		return nil, err
 	}
