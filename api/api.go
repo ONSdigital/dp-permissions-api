83481ae1d2bbd40339bdaef2450d93c47e586193
Pritesh Patel <40333998+p-patel@users.noreply.github.com>
2022-01-10 17:55:39 +0000

    Update api and models packages with types to use standardised success and error responses

--- api/api.go
+++ api/api.go
@@ -1,14 +1,13 @@
 package api
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 
-	"github.com/ONSdigital/dp-permissions-api/config"
-
 	"github.com/ONSdigital/dp-authorisation/v2/authorisation"
-
+	"github.com/ONSdigital/dp-permissions-api/config"
 	"github.com/ONSdigital/dp-permissions-api/models"
-
 	"github.com/gorilla/mux"
 )
 
@@ -20,6 +19,20 @@ type API struct {
 	defaultLimit        int
 	defaultOffset       int
 	maximumDefaultLimit int
+}
+
+type baseHandler func(ctx context.Context, w http.ResponseWriter, r *http.Request) (*models.SuccessResponse, *models.ErrorResponse)
+
+func contextAndErrors(h baseHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		ctx := req.Context()
+		response, err := h(ctx, w, req)
+		if err != nil {
+			writeErrorResponse(ctx, w, err)
+			return
+		}
+		writeSuccessResponse(ctx, w, response)
+	}
 }
 
 //Setup function sets up the api and returns an api
@@ -48,4 +61,47 @@ func Setup(
 	r.HandleFunc("/v1/permissions-bundle", api.GetPermissionsBundleHandler).Methods(http.MethodGet)
 
 	return api
+}
+
+func writeErrorResponse(ctx context.Context, w http.ResponseWriter, errorResponse *models.ErrorResponse) {
+	w.Header().Set("Content-Type", "application/json")
+	// process custom headers
+	if errorResponse.Headers != nil {
+		for key := range errorResponse.Headers {
+			w.Header().Set(key, errorResponse.Headers[key])
+		}
+	}
+	w.WriteHeader(errorResponse.Status)
+
+	jsonResponse, err := json.Marshal(errorResponse)
+	if err != nil {
+		responseErr := models.NewError(ctx, err, models.JSONMarshalError, models.ErrorMarshalFailedDescription)
+		http.Error(w, responseErr.Description, http.StatusInternalServerError)
+		return
+	}
+
+	_, err = w.Write(jsonResponse)
+	if err != nil {
+		responseErr := models.NewError(ctx, err, models.WriteResponseError, models.WriteResponseFailedDescription)
+		http.Error(w, responseErr.Description, http.StatusInternalServerError)
+		return
+	}
+}
+
+func writeSuccessResponse(ctx context.Context, w http.ResponseWriter, successResponse *models.SuccessResponse) {
+	w.Header().Set("Content-Type", "application/json")
+	// process custom headers
+	if successResponse.Headers != nil {
+		for key := range successResponse.Headers {
+			w.Header().Set(key, successResponse.Headers[key])
+		}
+	}
+	w.WriteHeader(successResponse.Status)
+
+	_, err := w.Write(successResponse.Body)
+	if err != nil {
+		responseErr := models.NewError(ctx, err, models.WriteResponseError, models.WriteResponseFailedDescription)
+		http.Error(w, responseErr.Description, http.StatusInternalServerError)
+		return
+	}
 }
