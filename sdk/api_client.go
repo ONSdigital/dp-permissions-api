a5ea1797ef626048f44c8469cb5cdc77fb886dc9
Haresh Kainth <haresh.kainth@methods.co.uk>
2022-11-09 01:36:05 +0000

    wip(1086):added implementations for missing endpoints. need to add unit tests.

--- sdk/api_client.go
+++ sdk/api_client.go
@@ -1,21 +1,25 @@
 package sdk
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	dphttp "github.com/ONSdigital/dp-net/v2/http"
+	"github.com/ONSdigital/dp-permissions-api/models"
+	"github.com/ONSdigital/log.go/v2/log"
 	"io"
 	"io/ioutil"
 	"net/http"
-	"time"
-
-	dphttp "github.com/ONSdigital/dp-net/v2/http"
-	"github.com/ONSdigital/log.go/v2/log"
 )
 
 // package level constants
 const (
-	bundlerEndpoint = "%s/v1/permissions-bundle"
+	bundlerEndpoint     = "%s/v1/permissions-bundle"
+	addPolicyEndpoint   = "%s/v1/policies"    // Add policy
+	policyEndpoint      = "%s/v1/policies/%s" // Delete policy
+	addRolesEndpoint    = "%s/v1/roles"       // Add roles
+	deleteRolesEndpoint = "%s/v1/roles/%s"    // Delete policy
 )
 
 // HTTPClient is the interface that defines a client for making HTTP requests
@@ -25,10 +29,9 @@ type HTTPClient interface {
 
 // APIClient implementation of permissions.Store that gets permission data from the permissions API
 type APIClient struct {
-	host            string
-	httpCli         HTTPClient
-	backoffSchedule []time.Duration
-	options         Options
+	host    string
+	httpCli HTTPClient
+	options Options
 }
 
 // Options is a struct containing for customised options for the API client
@@ -54,9 +57,172 @@ func NewClientWithClienter(host string, httpClient HTTPClient, opts Options) *AP
 	}
 }
 
+// == Roles Endpoint ==
+func (c *APIClient) GetAllRoles(ctx context.Context) (*models.Roles, error) {
+
+}
+
+func (c *APIClient) GetRole(ctx context.Context, id string) (*models.Roles, error) {
+
+}
+
+// == Policies Endpoint ==
+
+func (c *APIClient) AddPolicy(ctx context.Context, policy models.PolicyInfo) error {
+	uri := fmt.Sprintf(addPolicyEndpoint, c.host)
+
+	log.Info(ctx, "AddPolicy: starting permissions add policies request", log.Data{"uri": uri})
+
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(policy)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, uri, &buf)
+	if err != nil {
+		log.Info(ctx, "AddPolicy: error building new request", log.Data{"err": err.Error()})
+		return err
+	}
+
+	resp, err := c.httpCli.Do(ctx, req)
+	if err != nil {
+		log.Info(ctx, "AddPolicy: error executing request", log.Data{"err": err.Error()})
+		return err
+	}
+
+	defer func() {
+		if resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status returned from the permissions api permissions-addpolicies endpoint: %s", resp.Status)
+	}
+
+	log.Info(ctx, "AddPolicy: request successfully executed", log.Data{"resp.StatusCode": resp.StatusCode})
+
+	return nil
+}
+
+func (c *APIClient) DeletePolicy(ctx context.Context, id string) error {
+	uri := fmt.Sprintf(policyEndpoint, c.host, id)
+
+	log.Info(ctx, "DeletePolicy: starting permissions add policies request", log.Data{"uri": uri})
+
+	req, err := http.NewRequest(http.MethodDelete, uri, nil)
+	if err != nil {
+		log.Info(ctx, "DeletePolicy: error building new request", log.Data{"err": err.Error()})
+		return err
+	}
+
+	resp, err := c.httpCli.Do(ctx, req)
+	if err != nil {
+		log.Info(ctx, "DeletePolicy: error executing request", log.Data{"err": err.Error()})
+		return err
+	}
+
+	defer func() {
+		if resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	log.Info(ctx, "DeletePolicy: request successfully executed", log.Data{"resp.StatusCode": resp.StatusCode})
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status returned from the permissions api permissions-addpolicies endpoint: %s", resp.Status)
+	}
+
+	return nil
+}
+
+func (c *APIClient) GetPolicy(ctx context.Context, id string) (*models.Policy, error) {
+	uri := fmt.Sprintf(policyEndpoint, c.host, id)
+
+	log.Info(ctx, "GetPolicy: starting permissions get policy request", log.Data{"uri": uri})
+
+	req, err := http.NewRequest(http.MethodGet, uri, nil)
+	if err != nil {
+		log.Info(ctx, "GetPolicy: error building new request", log.Data{"err": err.Error()})
+		return nil, err
+	}
+
+	resp, err := c.httpCli.Do(ctx, req)
+	if err != nil {
+		log.Info(ctx, "GetPolicy: error executing request", log.Data{"err": err.Error()})
+		return nil, err
+	}
+
+	defer func() {
+		if resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status returned from the permissions api permissions-addpolicies endpoint: %s", resp.Status)
+	}
+
+	log.Info(ctx, "GetPolicy: request successfully executed", log.Data{"resp.StatusCode": resp.StatusCode})
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("unexpected error when attempting to read response: %v", err)
+	}
+
+	var result models.Policy
+	err = json.Unmarshal(b, &result)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal permission response to model: %v", err)
+	}
+
+	return &result, nil
+}
+
+func (c *APIClient) PutPolicy(ctx context.Context, id string, policy models.Policy) error {
+	uri := fmt.Sprintf(policyEndpoint, c.host, id)
+
+	log.Info(ctx, "PutPolicy: starting permissions get policy request", log.Data{"uri": uri})
+
+	b, err := json.Marshal(policy)
+	if err != nil {
+		log.Info(ctx, "PutPolicy: error executing request", log.Data{"err": err.Error()})
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPut, uri, bytes.NewReader(b))
+	if err != nil {
+		log.Info(ctx, "PutPolicy: error building new request", log.Data{"err": err.Error()})
+		return err
+	}
+
+	resp, err := c.httpCli.Do(ctx, req)
+	if err != nil {
+		log.Info(ctx, "PutPolicy: error executing request", log.Data{"err": err.Error()})
+		return err
+	}
+
+	defer func() {
+		if resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status returned from the permissions api permissions-addpolicies endpoint: %s", resp.Status)
+	}
+
+	log.Info(ctx, "PutPolicy: request successfully executed", log.Data{"resp.StatusCode": resp.StatusCode})
+
+	return nil
+}
+
+// == Permissions Endpoint ==
+
 // GetPermissionsBundle gets the permissions bundle data from the permissions API.
 func (c *APIClient) GetPermissionsBundle(ctx context.Context) (Bundle, error) {
-
 	uri := fmt.Sprintf(bundlerEndpoint, c.host)
 
 	log.Info(ctx, "GetPermissionsBundle: starting permissions bundle request", log.Data{"uri": uri})
